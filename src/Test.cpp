eaa0691e8fef19770510cb3c285edbc8d0bc984e
Bit Connor <bit@mutantlemon.com>
2011-12-14 03:13:06 +0200

    Add a test that reveals that the FileMultiTrie is actually broken!

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,6 +4,8 @@
 #include "MultiTrie.hpp"
 #include "FileMultiTrie.hpp"
 
+void testStdInput();
+
 int main()
 {
     MultiTrie trie;
@@ -32,4 +34,56 @@ int main()
     std::cout << fileTrie.getDocIds(createUString("dog"))[0] << std::endl;
     std::cout << fileTrie.getDocIds(createUString("dog"))[1] << std::endl;
     std::cout << fileTrie.getDocIds(createUString("doggy"))[0] << std::endl;
+
+    testStdInput();
+}
+
+// Pipe a file such as /usr/share/dict/words into stdin
+void testStdInput()
+{
+    MultiTrie trie;
+
+    std::cout << "Reading stdin..." << std::endl;
+
+    std::vector<std::string> allWords;
+
+    std::string word;
+    while(true)
+    {
+        std::getline(std::cin, word);
+        if(std::cin.eof())
+            break;
+        trie.add(word, 42);
+        allWords.push_back(word);
+    }
+
+    std::cout << "Writing to file..." << std::endl;
+
+    FileMultiTrie::WriteMultiTrie(trie, "words.trie");
+
+    std::cout << "Checking Memory trie..." << std::endl;
+
+    for(std::vector<std::string>::const_iterator i = allWords.begin(); i != allWords.end(); ++i)
+    {
+        std::vector<DocId> docs = trie.getDocIds(createUString(*i));
+        if(docs.size() != 1 || docs[0] != 42)
+        {
+            std::cout << "ERROR: " << *i << std::endl;
+        }
+    }
+
+    std::cout << "Checking File trie..." << std::endl;
+
+    FileMultiTrie fileTrie("words.trie");
+
+    for(std::vector<std::string>::const_iterator i = allWords.begin(); i != allWords.end(); ++i)
+    {
+        std::vector<DocId> docs = fileTrie.getDocIds(createUString(*i));
+        if(docs.size() != 1 || docs[0] != 42)
+        {
+            std::cout << "ERROR: " << *i << std::endl;
+        }
+    }
+
+    std::cout << "Done" << std::endl;
 }
